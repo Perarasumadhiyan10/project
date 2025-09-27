f5b3ba8aca874218a73561cef7392f718bbc1946
Sathish21 <sathishdeepak49@gmail.com>
2025-09-27 19:03:54 +0530

    display,update

--- display.php
+++ display.php
@@ -1,13 +1,17 @@
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style1.css">
 
 <?php
 include 'demo.html';
-$con = mysqli_connect("localhost","root","","webdesign");
+
+// 1. Database Connection
+$con = mysqli_connect("localhost", "root", "", "webdesign");
 
 if (!$con) {
     die("Connection Error: " . mysqli_connect_error());
 }
 
+// 2. SQL Query to select all customer records
 $sql = "SELECT * FROM customer";
 $res = mysqli_query($con, $sql);
 
@@ -15,40 +19,45 @@ if (!$res) {
     die("Query Failed: " . mysqli_error($con));
 }
 
-if (mysqli_num_rows($res) >= 0) {
-    echo "<center>";
+// 3. Display Table
+echo "<center><h2>Customer Records</h2>";
+if (mysqli_num_rows($res) > 0) {
     echo "<table border='1'>
-    <tr><th>Customer id</th>
-    <th>Full Name</th>
-    <th>Email id</th>
-    <th>Address</th>
-    <th>Product Name</th>
-    <th>Delivery Date</th>
-    <th>Payment</th>
-    <th>Contact No</th>
+    <tr>
+        <th>Customer ID</th>
+        <th>Full Name</th>
+        <th>Email ID</th>
+        <th>Address</th>
+        <th>Product Name</th>
+        <th>Delivery Date</th>
+        <th>Payment</th>
+        <th>Contact No</th>
+        <th>Action</th>
     </tr>";
-    while ($row = mysqli_fetch_assoc($res)) { 
 
-    echo "<tr>
-        <td>" .$row['customer id']."</td>
-        <td>" . $row['full name']."</td>
-       <td>" . $row['email id']." </td>
-      <td> " .$row['address'] ."</td>
-       <td>" . $row['product name']." </td>
-      <td>" . $row['deliver date']." </td>
-      <td> " .$row['payment'] ."</td>
-      <td>" . $row['contact number']." </td>
-    </tr>";
+    while ($row = mysqli_fetch_assoc($res)) {
+        // Use correct variable name consistently
+        $customer_id = $row['customer id'];
+
+        echo "<tr>
+            <td>" . $customer_id . "</td>
+            <td>" . $row['full name'] . "</td>
+            <td>" . $row['email id'] . "</td>
+            <td>" . $row['address'] . "</td>
+            <td>" . $row['product name'] . "</td>
+            <td>" . $row['deliver date'] . "</td>
+            <td>" . $row['payment'] . "</td>
+            <td>" . $row['contact number'] . "</td>
+            <td>
+                <a href='update.php?id=" . $customer_id . "'>Edit</a>
+            </td>
+        </tr>";
     }
-echo "</table>";
- echo "</center>";
+    echo "</table>";
 } else {
-    echo "NO records in the Table";
+    echo "No records in the table";
 }
+echo "</center>";
 
 mysqli_close($con);
 ?>
-
-
-
-
--- update.php
+++ update.php
@@ -1,6 +1,4 @@
-
-<link rel="stylesheet" href="style1.css">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style2.css">
 <?php
 include 'demo.html';
 $con = mysqli_connect("localhost", "root", "", "webdesign");
@@ -9,76 +7,62 @@ if (!$con) {
     die("Connection Error: " . mysqli_connect_error());
 }
 
-if (isset($_POST['update'])) {
-    $customer = $_POST['customer'];
-    $fname    = $_POST['fullname'];
-    $email    = $_POST['emailid'];
-    $address  = $_POST['address'];
-    $product  = $_POST['product'];
-    $date     = $_POST['date'];
-    $payment  = $_POST['payment'];
-    $number   = $_POST['number'];
-    
-    $sql = "UPDATE customer 
-            SET `full name` = '$fname', 
-                `email id` = '$email', 
-                `address` = '$address', 
-                `product name` = '$product', 
-                `deliver date` = '$date', 
-                `payment` = '$payment', 
-                `contact number` = '$number' 
-            WHERE `customer id` = '$customer'";
+// 2. Get customer ID from URL
+if (isset($_GET['id'])) {
+    $customer_id = $_GET['id'];
 
-$res = mysqli_query($con, $sql);
+    // 3. Fetch customer data
+    $sql = "SELECT * FROM customer WHERE `customer id` = '$customer_id'";
+    $res = mysqli_query($con, $sql);
 
-if ($res) {
-        $query = "SELECT * FROM customer WHERE `customer id` = '$customer'";
-        $result = mysqli_query($con, $query);
-        if ($row = mysqli_fetch_assoc($result)) {
-             echo "<center>";
-            echo "<table border='1'>
-                    <tr>
-                    <th>Customer ID</th>
-                        <th>Full Name</th>
-                        <th>Email ID</th>
-                        <th>Address</th>
-                        <th>Product Name</th>
-                        <th>Deliver Date</th>
-                        <th>Payment</th>
-                        <th>Contact Number</th>
-                    </tr>
-                    <tr>
-                        <td>{$row['customer id']}</td>
-                        <td>{$row['full name']}</td>
-                        <td>{$row['email id']}</td>
-                        <td>{$row['address']}</td>
-                        <td>{$row['product name']}</td>
-                        <td>{$row['deliver date']}</td>
-                        <td>{$row['payment']}</td>
-                        <td>{$row['contact number']}</td>
-                        </tr>
-                        </table>";
-                   echo "</center>";
-                } else {
-            echo "Customer not found after update.";
-        }
+    if (mysqli_num_rows($res) == 1) {
+        $row = mysqli_fetch_assoc($res);
     } else {
-        echo "Update failed: ";
+        echo "Customer not found.";
+        exit;
+    }
+} else {
+    echo "No customer ID provided.";
+    exit;
+}
+
+// 4. Handle form submission
+if (isset($_POST['update'])) {
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $product = $_POST['product'];
+    $date = $_POST['date'];
+    $payment = $_POST['payment'];
+    $contact = $_POST['contact'];
+
+    $update_sql = "UPDATE customer SET 
+        `full name` = '$fullname',
+        `email id` = '$email',
+        `address` = '$address',
+        `product name` = '$product',
+        `deliver date` = '$date',
+        `payment` = '$payment',
+        `contact number` = '$contact'
+        WHERE `customer id` = '$customer_id'";
+
+    if (mysqli_query($con, $update_sql)) {
+        echo "<script>alert('Customer updated successfully'); window.location.href='display.php';</script>";
+    } else {
+        echo "Update failed: " . mysqli_error($con);
     }
 }
-mysqli_close($con);
 ?>
-<div class="customer">
-    <form method="post" action="">
-        Customer ID: <input type="text" name="customer"><br><br>
-        Full Name: <input type="text" name="fullname"><br><br>
-        Email: <input type="text" name="emailid"><br><br>
-        Address: <input type="text" name="address"><br><br>
-        Product Name: <input type="text" name="product"><br><br>
-        Delivery Date: <input type="date" name="date"><br><br>
-        Payment: <input type="text" name="payment"><br><br>
-       Contact Number: <input type="text" name="number"><br><br>
-        <center><button type="submit" name="update">Update</button></center>
-    </form>
-</div>
 
+<!-- 5. Display Edit Form -->
+<form method="post">
+    <h2>Edit Customer</h2>
+    Full Name: <input type="text" name="fullname" value="<?php echo $row['full name']; ?>"><br><br>
+    Email ID: <input type="email" name="email" value="<?php echo $row['email id']; ?>"><br><br>
+    Address: <input type="text" name="address" value="<?php echo $row['address']; ?>"><br><br>
+    Product Name: <input type="text" name="product" value="<?php echo $row['product name']; ?>"><br><br>
+    Delivery Date: <input type="date" name="date" value="<?php echo $row['deliver date']; ?>"><br><br>
+    Payment: <input type="text" name="payment" value="<?php echo $row['payment']; ?>"><br><br>
+    Contact No: <input type="text" name="contact" value="<?php echo $row['contact number']; ?>"><br><br>
+  <button type="submit">Update</button>
+</form>
